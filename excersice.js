3ff196de06ce0f8208ff0dfadafd9aaf5c17e4c5
hadep275 <hadep275@gmail.com>
2023-04-10 23:06:29 -0600

    install react

--- excersice.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello!")
-console.log("LA VIDA LOCA")
-console.log("From Hikmah")
-console.log("wherever we seem to be together")
-
-console.log("what is going on?")
-console.log("hips don't lie")
-console.log("Han: What are your guys' favourite snacks? pls answer here")
-console.log ("Merci hazelnut chocolate - Hikmah")
---
+++ frontend/main.jsx
@@ -0,0 +1,13 @@
+import react from "react"
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const main = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default main
